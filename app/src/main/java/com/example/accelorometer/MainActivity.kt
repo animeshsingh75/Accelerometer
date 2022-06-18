e9a993fd13be4887dc97f20590108492fc28e129
Animesh Singh <animesh.singh@myparkplus.com>
2022-06-18 13:45:32 +0530

    First commit

---
+++ app/src/main/java/com/example/accelorometer/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.accelorometer
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.abs
+
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private var sensorManager: SensorManager? = null
+
+    var prevX: Float? = null
+    var prevY: Float? = null
+    var prevZ: Float? = null
+    var diffX: Float? = null
+    var diffY: Float? = null
+    var diffZ: Float? = null
+    override fun onAccuracyChanged(s: Sensor?, i: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(event)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    private fun getAccelerometer(event: SensorEvent) {
+        // Movement
+        val xVal = event.values[0]
+        val yVal = event.values[1]
+        val zVal = event.values[2]
+        val tvXAxis=findViewById<TextView>(R.id.tvXAxis)
+        val tvYAxis=findViewById<TextView>(R.id.tvYAxis)
+        val tvZAxis=findViewById<TextView>(R.id.tvZAxis)
+        if (prevX == null) {
+            prevX = xVal
+            prevY = yVal
+            prevZ = zVal
+            diffX = 0F
+            diffY = 0F
+            diffZ = 0F
+        } else {
+            if (diffX!! < abs(xVal - prevX!!)) {
+                diffX = abs(xVal - prevX!!)
+                tvXAxis.text=diffX.toString()
+            }
+            if (diffY!! < abs(yVal - prevY!!)) {
+                diffY = abs(yVal - prevY!!)
+                tvYAxis.text=diffY.toString()
+            }
+            if (diffZ!! < abs(zVal - prevZ!!)) {
+                diffZ = abs(zVal - prevZ!!)
+                tvZAxis.text=diffZ.toString()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager?.registerListener(
+            this,
+            sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager?.unregisterListener(this)
+    }
+}
