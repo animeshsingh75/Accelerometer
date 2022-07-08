cb552be32f21a3e840b74d73f88674db237fa856
Animesh Singh <animesh.singh@myparkplus.com>
2022-07-08 18:32:27 +0530

    Commit

--- app/src/main/java/com/example/accelorometer/MainActivity.kt
+++ app/src/main/java/com/example/accelorometer/MainActivity.kt
@@ -9,25 +9,24 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     private var sensorManager: SensorManager? = null
-
-    var prevX: Float? = null
-    var prevY: Float? = null
-    var prevZ: Float? = null
-    var diffX: Float? = null
-    var diffY: Float? = null
-    var diffZ: Float? = null
+    var maxX=0F
+    var maxY=0F
+    var maxZ=0F
     override fun onAccuracyChanged(s: Sensor?, i: Int) {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
-            getAccelerometer(event)
-        }else if (event?.sensor?.type==Sensor.TYPE_GYROSCOPE){
-            getGyro(event)
+//        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+//            getAccelerometer(event)
+//        }
+        if(event?.sensor?.type==Sensor.TYPE_GYROSCOPE){
+            getGyroscope(event)
         }
     }
 
@@ -38,63 +37,59 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
 
-    private fun getAccelerometer(event: SensorEvent) {
-        // Movement
-        val xVal = event.values[0]
-        val yVal = event.values[1]
-        val zVal = event.values[2]
-//        val tvXAxis=findViewById<TextView>(R.id.tvXAxis)
-//        val tvYAxis=findViewById<TextView>(R.id.tvYAxis)
-//        val tvZAxis=findViewById<TextView>(R.id.tvZAxis)
-        val currentXAxis=findViewById<TextView>(R.id.currentXAxis)
-        val currentYAxis=findViewById<TextView>(R.id.currentYAxis)
-        val currentZAxis=findViewById<TextView>(R.id.currentZAxis)
-        currentXAxis.text=xVal.toString()
-        currentYAxis.text=yVal.toString()
-        currentZAxis.text=zVal.toString()
-//        if (prevX == null) {
-//            prevX = xVal
-//            prevY = yVal
-//            prevZ = zVal
-//            diffX = 0F
-//            diffY = 0F
-//            diffZ = 0F
-//        } else {
-//            if (diffX!! < abs(xVal - prevX!!)) {
-//                diffX = abs(xVal - prevX!!)
-//                tvXAxis.text=diffX.toString()
-//            }
-//            if (diffY!! < abs(yVal - prevY!!)) {
-//                diffY = abs(yVal - prevY!!)
-//                tvYAxis.text=diffY.toString()
-//            }
-//            if (diffZ!! < abs(zVal - prevZ!!)) {
-//                diffZ = abs(zVal - prevZ!!)
-//                tvZAxis.text=diffZ.toString()
-//            }
+//    private fun getAccelerometer(event: SensorEvent) {
+//        // Movement
+//        val xVal = event.values[0]
+//        val yVal = event.values[1]
+//        val zVal = event.values[2]
+//        val currentXAxis = findViewById<TextView>(R.id.currentXAxis)
+//        val currentYAxis = findViewById<TextView>(R.id.currentYAxis)
+//        val currentZAxis = findViewById<TextView>(R.id.currentZAxis)
+//        val tvXAxis = findViewById<TextView>(R.id.tvXAxis)
+//        val tvYAxis = findViewById<TextView>(R.id.tvYAxis)
+//        val tvZAxis = findViewById<TextView>(R.id.tvZAxis)
+//        currentXAxis.text = xVal.toString()
+//        currentYAxis.text = yVal.toString()
+//        currentZAxis.text = zVal.toString()
+//        val a = sqrt(xVal.pow(2) + yVal.pow(2) + zVal.pow(2))
+//        val g = a / 9.8
+//        tvXAxis.text = a.toString()
+//        tvYAxis.text = g.toString()
+//        if(g>max){
+//            max=g
 //        }
-    }
-
-    private fun getGyro(event: SensorEvent) {
+//        tvZAxis.text=max.toString()
+//    }
+    private fun getGyroscope(event: SensorEvent) {
         // Movement
         val xVal = event.values[0]
         val yVal = event.values[1]
         val zVal = event.values[2]
-        val tvXAxis=findViewById<TextView>(R.id.tvXAxis)
-        val tvYAxis=findViewById<TextView>(R.id.tvYAxis)
-        val tvZAxis=findViewById<TextView>(R.id.tvZAxis)
-        tvXAxis.text=xVal.toString()
-        tvYAxis.text=yVal.toString()
-        tvZAxis.text=zVal.toString()
+        val currentXAxis = findViewById<TextView>(R.id.currentXAxis)
+        val currentYAxis = findViewById<TextView>(R.id.currentYAxis)
+        val currentZAxis = findViewById<TextView>(R.id.currentZAxis)
+        val tvXAxis = findViewById<TextView>(R.id.tvXAxis)
+        val tvYAxis = findViewById<TextView>(R.id.tvYAxis)
+        val tvZAxis = findViewById<TextView>(R.id.tvZAxis)
+        currentXAxis.text = xVal.toString()
+        currentYAxis.text = yVal.toString()
+        currentZAxis.text = zVal.toString()
+        if(abs(xVal)>maxX){
+            maxX=abs(xVal)
+            tvXAxis.text=maxX.toString()
+        }
+        if(abs(yVal)>maxY){
+            maxY=abs(yVal)
+            tvYAxis.text=maxY.toString()
+        }
+        if(abs(zVal)>maxZ){
+            maxZ=abs(zVal)
+            tvZAxis.text=maxZ.toString()
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        sensorManager?.registerListener(
-            this,
-            sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
         sensorManager?.registerListener(
             this,
             sensorManager?.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
